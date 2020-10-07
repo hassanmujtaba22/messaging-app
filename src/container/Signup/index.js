c99be7690ac3ddde9d234a8ecf70a104bf8f703f
hassanmujtaba <hassan03203406130@gmail.com>
2020-10-07 23:07:20 +0500

    signup page created and functionality added

--- src/container/Signup/index.js
+++ src/container/Signup/index.js
@@ -1,30 +1,45 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Card from "../../components/Card"
 import "./style.css"
+import { signup } from "../../actions"
+import { useDispatch } from 'react-redux'
+
 function Signup() {
     const [fname, setFname] = useState("")
     const [lname, setLname] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+
+    const dispatch = useDispatch()
+    
+    const registerUser = (e) => {
+
+        e.preventDefault();
+
+        const user = { fname, lname, email, password }
+        dispatch(signup(user))
+
+    }
+
     return (
         <div class="signup">
             <Card>
                 <h3>Sign Up</h3>
-                <form>
+                <form onSubmit={registerUser}>
                     <input
                         name="fname"
                         type="text"
                         placeholder="First Name"
                         value={fname}
-                        onChange={(e) => setFname  (e.target.value)}
+                        onChange={(e) => setFname(e.target.value)}
                     />
-                    
+
                     <input
                         name="lname"
                         type="text"
                         placeholder="Last Name"
                         value={lname}
-                        onChange={(e) => setLname  (e.target.value)}
+                        onChange={(e) => setLname(e.target.value)}
                     />
 
                     <input
@@ -44,7 +59,7 @@ function Signup() {
                     />
 
                     <div>
-                        <button>Submit</button>
+                        <button type="submit">Submit</button>
                     </div>
 
                 </form>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from "firebase";
+import { Provider } from 'react-redux';
+import store from './store';
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYmO1MjY6JHqMupNUR0c7EmGxS2FmpcjU",
+  authDomain: "chat-app-9f097.firebaseapp.com",
+  databaseURL: "https://chat-app-9f097.firebaseio.com",
+  projectId: "chat-app-9f097",
+  storageBucket: "chat-app-9f097.appspot.com",
+  messagingSenderId: "1011357501650",
+  appId: "1:1011357501650:web:3408daef344f4c27ef850b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+window.store = store
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
